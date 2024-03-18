4e670bb3c01c3f3a3c0c8cbeee82682195941964
Monirul Islam <mdmonirulislammonir@gmail.com>
2024-03-18 20:12:31 +0600

    the header.php

--- index.php
+++ index.php
@@ -1,12 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>WP Hierarchy</title>
-</head>
-<body>
-    
+  <?php get_header()?>
 
  <h1>index.php</h1>
 
